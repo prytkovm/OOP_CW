0b4d1618443d1cf3a1ec3db388a06b645d422790
prytkovm <maksim.prytkov.2002@gmail.com>
2023-05-20 21:20:16 +0300

    user change

--- src/Station.cpp
+++ src/Station.cpp
@@ -1,5 +1,6 @@
 #include "Station.h"
 
+
 Station::Station(std::vector<User> &usersList, QObject *parent) : QObject(parent), users(usersList) {
 
 }
--- src/User.cpp
+++ src/User.cpp
@@ -1,6 +1,7 @@
 #include "User.h"
+#include <iostream>
 
-int User::objectsCount;
+int User::objectsCount = 0;
 
 User::User(QObject *parent) : QObject(parent) {
     state = AllowedStates::READY;
@@ -20,7 +21,7 @@ std::string User::generateNumber() {
     using std::string, std::to_string;
 
     auto strNumber = to_string(User::objectsCount);
-    auto result = std::string("0", (numberLength - strNumber.length()));
+    auto result = std::string((numberLength - strNumber.length()), '0') + strNumber;
 
-    return result + strNumber;
+    return result;
 }
