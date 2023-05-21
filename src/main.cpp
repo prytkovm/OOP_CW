62650caf7cbd799450406d4f162d5e6c021416ab
prytkovm <maksim.prytkov.2002@gmail.com>
2023-05-21 22:43:56 +0300

    Fixed states changes

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,19 @@
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    auto w = new MainWindow();
-    w->show();
+    MainWindow window;
+    window.show();
     std::vector<User*> users;
     for (auto i = 0; i < usersLimit; i++) {
         auto user = new User(&a);
-        auto card = new UserCard(user, w);
-        w->addUserCard(card);
+        auto card = new UserCard(user, &window);
+        window.addUserCard(card);
         users.push_back(user);
     }
     Station station(users);
-    return QApplication::exec();
+    int exitCode = QApplication::exec();
+    for (auto &user: users) {
+        delete user;
+    }
+    return exitCode;
 }
